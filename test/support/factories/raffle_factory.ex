2a928b009ba42c45c9d8212a208ce525841a3d51
William Jager <williamjager.dev@gmail.com>
2022-12-24 02:19:46 -0300

    chore: fix following credo

--- test/support/factories/raffle_factory.ex
+++ test/support/factories/raffle_factory.ex
@@ -1,12 +1,15 @@
 defmodule Lottery.RaffleFactory do
   @moduledoc false
 
+  alias Faker.{Date, Person.PtBr}
+  alias Lottery.Events.Raffle
+
   defmacro __using__(_opts) do
     quote do
       def raffle_factory(attrs) do
-        %Lottery.Events.Raffle{
-          name: Faker.Person.PtBr.name(),
-          raffle_date: Faker.Date.forward(7)
+        %Raffle{
+          name: PtBr.name(),
+          raffle_date: Date.forward(7)
         }
         |> Map.merge(attrs)
       end
